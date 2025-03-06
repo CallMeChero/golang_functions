16a95d13043f6efb10a42399964808fbba2db0b3
CallMeChero <david.ceranic@gmail.com>
2025-03-06 18:15:08 +0100

    Using variadic fn

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	fact := factorial(5)
+	fmt.Println(fact)
+
+	sum := variadicFn(1, 15, 20, 50, 10)
+
+	fmt.Println(sum)
+}
+
+func factorial(number int) int {
+	if number == 1 {
+		return 1
+	}
+	return number * factorial(number-1)
+	// result := 1
+
+	// for i := 1; i <= number; i++ {
+	// 	result = result * i
+	// }
+
+	// return result
+}
+
+func variadicFn(startingValue int, numbers ...int) int {
+	sum := 0
+
+	for _, val := range numbers {
+		sum += val
+	}
+
+	return sum
 }
